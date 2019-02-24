1d763a054ceb2d3cec7daa0ab32cf3d222b687a0
马蕾 <18ericz@18ericzdemacbook-pro.local>
2019-02-24 14:34:09 -0500

    Version 6.2 --> Lacks stack view, does not work

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var fromUnitLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var formulaPicker: UIPickerView!
+    
+    var formulaArray = ["Miles to Kilometer","Kilometer to Miles","Feets to meters", "Yards to meters","Meters to feets","Meters to Yards"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        formulaPicker.delegate = self
+        formulaPicker.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return formulaArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return formulaArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fromUnitLabel.text = formulaArray[row]
+    }
+}
